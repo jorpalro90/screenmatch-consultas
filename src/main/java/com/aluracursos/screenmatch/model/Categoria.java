70e1f285f7d2ca0c492aa979961bea36f8cb780c
jorpalro90 <jorgel.palacior@outlook.com>
2024-11-08 22:22:17 -0500

    aplicación para buscar series y episodios desde api

---
+++ src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.aluracursos.screenmatch.model;
+
+public enum Categoria {
+    ACCION("Action", "Acción"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comedia"),
+    DRAMA("Drama", "Drama"),
+    CRIMEN("Crime", "Crimen"),
+    OTROS("Others", "Otros");
+
+    private String categoriaOmdb;
+    private String categoriaEspanol;
+
+    Categoria(String categoriaOmdb, String categoriaEspanol) {
+        this.categoriaOmdb = categoriaOmdb;
+        this.categoriaEspanol = categoriaEspanol;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        System.out.println("Categoría desconocida: " + text);
+        return OTROS;
+    }
+
+    public static Categoria fromEspanol(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaEspanol.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        System.out.println("Categoría desconocida: " + text);
+        return OTROS;
+    }
+}
